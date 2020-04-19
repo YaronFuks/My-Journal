10f775413fd8f7373244ee350d76170b66e00372
Yaron Fuks <yaronfuks@gmail.com>
2020-04-20 00:23:55 +0300

    My Journal

--- app/src/main/java/com/yaronfuks/myjournal/PostJournalActivity.java
+++ app/src/main/java/com/yaronfuks/myjournal/PostJournalActivity.java
@@ -3,10 +3,22 @@ package com.yaronfuks.myjournal;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
@@ -33,6 +45,9 @@ import com.google.firebase.storage.UploadTask;
 import com.yaronfuks.myjournal.Util.JournalApi;
 import com.yaronfuks.myjournal.model.Journal;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Objects;
 
@@ -40,6 +55,8 @@ import java.util.Objects;
 public class PostJournalActivity extends AppCompatActivity implements View.OnClickListener {
     private static final int GALLERY_CODE = 1;
     private static final String TAG = "PostJournalActivity";
+    public static final int CAMERA_PERM_CODE = 101;
+    public static final int CAMERA_REQUEST_CODE = 102;
     private Button saveButton;
     private ProgressBar progressBar;
     private ImageView addPhotoButton;
@@ -47,6 +64,7 @@ public class PostJournalActivity extends AppCompatActivity implements View.OnCli
     private EditText thoughtsEditText;
     private TextView currentUserTextView;
     private ImageView imageView;
+    private String currentPhotoPath;
 
 
     private String currentUserId;
@@ -88,8 +106,6 @@ public class PostJournalActivity extends AppCompatActivity implements View.OnCli
         progressBar.setVisibility(View.INVISIBLE);
 
 
-
-
         if (JournalApi.getInstance() != null) {
             currentUserId = JournalApi.getInstance().getUserId();
             currentUserName = JournalApi.getInstance().getUsername();
@@ -113,9 +129,39 @@ public class PostJournalActivity extends AppCompatActivity implements View.OnCli
 
     }
 
-    public void hideKey(View view){
+    public void hideKey(View view) {
         InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
         inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+    }
+
+
+    private void selectImage(Context context) {
+        final CharSequence[] options = { "Take Photo", "Choose from Gallery","Cancel" };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle("Choose your thought picture");
+
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+
+                if (options[item].equals("Take Photo")) {
+                   askCameraPermissions();
+//                    Intent takePicture = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+//                    startActivityForResult(takePicture, 0);
+
+                } else if (options[item].equals("Choose from Gallery")) {
+                    choosePhotoFromGallery();
+//                    Intent pickPhoto = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+//                    startActivityForResult(pickPhoto , 1);
+
+                } else if (options[item].equals("Cancel")) {
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
     }
 
     @Override
@@ -126,14 +172,46 @@ public class PostJournalActivity extends AppCompatActivity implements View.OnCli
                 saveJournal();
                 break;
             case R.id.postCameraButton:
+
+                selectImage(this);
+//                askCameraPermissions();
+
                 //get image from gallery/phone
-                Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
-                galleryIntent.setType("image/*");
-                startActivityForResult(galleryIntent, GALLERY_CODE);
+//                Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+//                galleryIntent.setType("image/*");
+//                startActivityForResult(galleryIntent, GALLERY_CODE);
                 break;
         }
 
     }
+
+    private void choosePhotoFromGallery(){
+        //get image from gallery/phone
+        Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent, GALLERY_CODE);
+    }
+
+    private void askCameraPermissions() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, CAMERA_PERM_CODE);
+        } else {
+            dispatchTakePictureIntent();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == CAMERA_PERM_CODE) {
+            if (grantResults.length < 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                dispatchTakePictureIntent();
+            } else {
+                Toast.makeText(this, "Camera Permissions is Required to use camera", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
 
     private void saveJournal() {
         final String title = titleEditText.getText().toString().trim();
@@ -221,6 +299,15 @@ public class PostJournalActivity extends AppCompatActivity implements View.OnCli
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA_REQUEST_CODE) {
+           if (resultCode == Activity.RESULT_OK){
+               File f = new File(currentPhotoPath);
+               imageUri = Uri.fromFile(f);
+               imageView.setImageURI(Uri.fromFile(f));
+
+           }
+        }
+
         if (requestCode == GALLERY_CODE && resultCode == RESULT_OK) {
             if (data != null) {
                 imageUri = data.getData(); // we have the actual path to the image
@@ -229,6 +316,48 @@ public class PostJournalActivity extends AppCompatActivity implements View.OnCli
             }
         }
     }
+
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "my_image_" + timeStamp;
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.yaronfuks.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, CAMERA_REQUEST_CODE);
+            }
+        }
+    }
+
 
     @Override
     protected void onStart() {
@@ -245,5 +374,4 @@ public class PostJournalActivity extends AppCompatActivity implements View.OnCli
         }
     }
 }
-
 
